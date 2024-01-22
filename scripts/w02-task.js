cbbcea320b77e0069c93a9a1d70f028d36fef312
Jtenna <eguareone@gmail.com>
2024-01-22 07:14:05 +0100

    Added new changes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,7 +3,6 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
 // Declaring Fullname 
 let fullName = 'Justina Eguare';
 // Declaring current year
@@ -13,9 +12,14 @@ const profilePicture = 'images/justina.JPG';
 
 
 /* Step 3 - Element Variables */
-
-
-
+// 'name' Id element
+const nameElement = document.getElementById('name');
+// 'food' Id element
+const foodElement = document.getElementById('food');
+// 'year' Id selector 
+const yearElement = document.querySelector('#year');
+// 'image' id element 
+const imageElement = document.querySelector('picture')
 
 
 /* Step 4 - Adding Content */
