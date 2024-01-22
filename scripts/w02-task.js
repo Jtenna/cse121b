ab0d4d2699e7e382422e6ffbd86bebf3987388cf
Jtenna <eguareone@gmail.com>
2024-01-22 06:44:19 +0100

    Added new details

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,11 +1,15 @@
 /* W02-Task - Profile Home Page */
-const fullName = 'Justina Eguare';
 
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
 
-
+// Declaring Fullname 
+let fullName = 'Justina Eguare';
+// Declaring current year
+let currentDate = new Date('2024');
+// Declaring the filepath
+const profilePicture = 'images/justina.JPG';
 
 
 /* Step 3 - Element Variables */
