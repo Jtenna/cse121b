bbd1a38f0a3af5791e9dba39574d35c02bdf3a90
Jtenna <eguareone@gmail.com>
2024-02-12 23:58:32 +0100

    new changes

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,11 +1,24 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-
+const templesElement = document.querySelector('#temples');
+let templeList = [];
 
 /* async displayTemples Function */
-
-
+const displayTemples = (temples) => {
+    temples.forEach((temple) => {
+      const article = document.createElement('article');
+      const h3 = document.createElement('h3');
+      h3.textContent = temple.templeName;
+      const img = document.createElement('img');
+      img.src = temple.imageUrl;
+      img.alt = temple.location;
+      article.appendChild(h3);
+      article.appendChild(img);
+      templesElement.appendChild(article);
+      // Add article to the DOM or do something else with it
+    });
+  };
 /* async getTemples Function using fetch()*/
 
 
