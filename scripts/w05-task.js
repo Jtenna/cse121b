1a9036dca59f573ce3cc61ddc946c829d5fbb07c
Jtenna <eguareone@gmail.com>
2024-02-13 12:34:25 +0100

    changed 'nonutah' to 'notutah' in the filter case

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -44,19 +44,20 @@ const filterTemples = (temples) => {
   const filter = document.querySelector('#filtered').value;
   switch (filter) {
     case 'utah':
-      displayTemples(temples.filter((temple) => temple.location.includes('Utah')));
+      displayTemples(temples.filter(temple => temple.location.includes('Utah')));
       break;
-    case 'nonutah':
-      displayTemples(temples.filter((temple) => !temple.location.includes('Utah')));
+    
+    case 'notutah':
+      displayTemples(temples.filter(temple => !temple.location.includes('Utah')));
       break;
     case 'older':
-      displayTemples(temples.filter((temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+      displayTemples(temples.filter (temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
       break;
     case 'all':
       displayTemples(temples);
       break;
     default:
-      displayTemples(temples);
+      ;
   }
 };
 
